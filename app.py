0242b6716b95ee767eef6d1766552c74de499207
Pepper <56687049+echocall@users.noreply.github.com>
2025-01-14 19:02:57 -0500

    First Steps
    assets\ROOMS
    - Created example room JSON files & folder for Shrines
    
    assets\UNITS
     - Created example unit JSON files
    
    app.py
     - Testing os.walk to navigate through filedirectory and subdirectories for future implementation
    - Added comments to remember what to do next

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+assets_file_path = 'C:\\Users\\strip\\Documents\\github\\STAT\\assets'
+timesRun = 0
+
+# Use Strategy design strategy to pick which filepath to use: UNITS, ROOMS, or IMAGES
+# Run the iterative forloop for each of thos SEPARATELY to write files into list.
+for directory, subdirlist, filelist in os.walk(assets_file_path):
+    print(directory)
+    print(subdirlist) 
+    for f in filelist:
+        print('\t' + f)
+
+# Order of operations: Fetch information from ROOMS or UNITS folders and throw into list
+# Rewrite list to account for anything with the same RoomType or UnitType into its own mini-list inside the big list.
+# Use the Rewritten list to generate the 'cards' in the GUI with the multiple objects of same Type being 'stacked' or opening when clicked.
+
+# ???
+
+# Profit!!!
