6ed7232d60eb04cbb00531b5ac4d55cc7af81fc1
Pepper <56687049+echocall@users.noreply.github.com>
2025-01-16 16:34:20 -0500

    Summary
    - Made more mock-up rooms for testing.
    
    app.py
     - Testing Path for trawling through files.

--- app.py
+++ app.py
@@ -1,14 +1,47 @@
 import os
+from pathlib import Path
+
 assets_file_path = 'C:\\Users\\strip\\Documents\\github\\STAT\\assets'
 timesRun = 0
 
 # Use Strategy design strategy to pick which filepath to use: UNITS, ROOMS, or IMAGES
 # Run the iterative forloop for each of thos SEPARATELY to write files into list.
-for directory, subdirlist, filelist in os.walk(assets_file_path):
+''' for directory, subdirlist, filelist in os.walk(assets_file_path):
     print(directory)
     print(subdirlist) 
     for f in filelist:
-        print('\t' + f)
+        print('\t' + f) '''
+
+p = Path('C:\\Users\\strip\\Documents\\github\\STAT\\assets\\ROOMS')
+subdirectories = [x for x in p.iterdir() if x.is_dir()]
+print(subdirectories)
+
+directory = Path('C:\\Users\\strip\\Documents\\github\\STAT\\assets')
+
+
+# splits subdirectories and create lists based on them. 
+for subdirectory in subdirectories:
+    # Get the directories, go to each directory & write the contents to a list
+    # If the directory contains a subdirectory: create a new list to hold the contents of the subdirectory
+    # append new list to the outer list.
+    # Final Product: Buffing[dorms,shrines[defense,fortune]]
+    
+    # 
+
+    print("nope")
+
+
+
+path = Path('C:\\Users\\strip\\Documents\\github\\STAT\\assets\\ROOMS')
+jsonslist = list(path.glob('**/*.json'))
+#print(jsonslist)
+
+units = []
+
+for path in jsonslist:
+    units.append(path.read_text())
+
+
 
 # Order of operations: Fetch information from ROOMS or UNITS folders and throw into list
 # Rewrite list to account for anything with the same RoomType or UnitType into its own mini-list inside the big list.
