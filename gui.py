e9d4fd1d84ea5903a37462ee0f5024fcb3bc5d8c
Pepper <56687049+echocall@users.noreply.github.com>
2025-03-19 20:12:21 -0400

    Update gui.py
    preparing to mess around with nicegui

--- gui.py
+++ gui.py
@@ -1,47 +1,10 @@
-from tkinter import *
+from nicegui import app, ui
 
-win=Tk() #creating the main window and storing the window object in 'win'
-win.title('STAT: Snazzy Tabletop Assistant Tracker')
-win.geometry('350x525')
+app.native.window_args['resizable'] = False
+app.native.start_args['debug'] = True
+app.native.settings['ALLOW_DOWNLOADS'] = True
 
-mn = Menu(win) 
-win.config(menu=mn) 
+ui.label('app running in native mode')
+ui.button('enlarge', on_click=lambda: app.native.main_window.resize(1000, 700))
 
-pw = PanedWindow()
-
-file_menu = Menu(mn) 
-mn.add_cascade(label='File', menu=file_menu, activebackground="blue") 
-file_menu.add_command(label='Create Game') 
-file_menu.add_command(label='Load Game') 
-file_menu.add_separator() 
-file_menu.add_command(label='New Save') 
-file_menu.add_command(label='Load Save')  
-file_menu.add_separator() 
-file_menu.add_command(label='Exit', command=win.quit) 
-
-help_menu = Menu(mn) 
-mn.add_cascade(label='Help', menu=help_menu) 
-help_menu.add_command(label='Tutorials')
-help_menu.add_command(label='Feedback') 
-help_menu.add_command(label='Contact') 
-
-pw.pack(fill = 'both', expand = 1)
-w1 = Scrollbar(win)
-w1.pack(side = RIGHT, fill = 'y')
-list_1 = Listbox(win, yscrollcommand = w1.set)
-for i in range(20):
-    list_1.insert(END, 'ListItem ' + str(i))
-list_1.pack( side = LEFT, fill = BOTH)
-w1.config( command = list_1.yview)
-
-pw.add(w1)
-
-
-tl= Toplevel() 
-tl.title('Greetings') 
-txt=Text(tl, heigh=5, width=45)
-txt.pack()
-txt.insert(INSERT,"Hello!\n")
-txt.insert(END, 'We begin the program here. :)\n')
-
-win.mainloop() #running the loop that works as a trigger
+ui.run(native=True, window_size=(400, 300), fullscreen=False)
