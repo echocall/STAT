a4e083dae29f53b2c253e48fe7bfbb73a841d7b4
Pepper <56687049+echocall@users.noreply.github.com>
2025-04-15 10:17:13 -0400

    Summary
    Testing ways to implement a way for the user to pick files/filepaths

--- main2.py
+++ main2.py
@@ -1,19 +1,10 @@
-from nicegui import ui
+from nicegui import app, ui
 
-def add_chip():
-    with chips:
-        ui.chip(label_input.value, icon='label', color='silver')
-    label_input.value = ''
+async def choose_file():
+    files = await app.native.main_window.create_file_dialog(allow_multiple=True)
+    for file in files:
+        ui.notify(file)
 
-label_input = ui.input('Add label').on('keydown.enter', add_chip)
-with label_input.add_slot('append'):
-    ui.button(icon='add', on_click=add_chip).props('round dense flat')
+ui.button('choose file', on_click=choose_file)
 
-with ui.row().classes('gap-0') as chips:
-    ui.chip('Label 1', icon='label', color='silver', removable=True)
-
-ui.button('Restore removed chips', icon='unarchive',
-          on_click=lambda: [chip.set_value(True) for chip in chips]) \
-    .props('flat')
-
-ui.run()
+ui.run(native=True)
