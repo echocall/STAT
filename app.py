2a761e128ae9f93841e5068116b92f7f12142baf
Pepper <56687049+echocall@users.noreply.github.com>
2025-04-19 11:36:17 -0400

    Summary
    Testing for a new folder structure for future.
     - not as bad as I thought.

--- app.py
+++ app.py
@@ -1,46 +1,38 @@
 import traceback
-from helpers.crud import *
-from helpers.utilities import *
-from handlers.assethandler import *
-from handlers.gamehandler import *
-from handlers.savehandler import *
-from classes.StatInstance import *
-from classes.MyGame import *
-from tests import *
+from pathlib import Path, PurePath
 
-# Initialzie the class instance.
-currentStat = MyStat("config.txt")
-print("Is game loaded: ")
-print(currentStat.is_game_loaded)
-print("Game loaded: ")
-print(currentStat.game_loaded_name)
-print()
+print("in app.py")
 
-# filePaths = stat_initialize(currentStat.config_file)
-# print(filePaths)
+print('===============================')
+passedDirectoryPath = "C:/Users/strip/Documents/github/STAT/statassets/games"
+objectType = 'game'
+error_message = ""
+unsplit_objects = []
+object_names = []
+str_directory_path = passedDirectoryPath
+fetch_success = False
 
-check_load_config = load_config(currentStat.config_file)
-print(check_load_config)
-print()
-print()
+# casting to Path 
+directory_path = Path(str_directory_path)
 
+if directory_path.exists():
+    # check each directory in the directory for **/*.json file
+    jsonslist = sorted(Path(directory_path).glob('*/*.json'))
+    
+    # for each json file in the list, get the name.
+    for x in (jsonslist):
+        y = PurePath(x)
+        unsplit_objects.append(y.name)
+    fetch_success = True
+    # Split the names
+    for object in unsplit_objects:
+        name = object.split(".")
+        object_names.append(name[0])
+else:
+      # TODO: change to pass error_message back
+    print("Incorrect Path: " + objectType + " directory for names not found!")
 
-# === NEW GAME ====
-
-game_template = single_json_getter('template_game','C:\\Users\\strip\\Documents\\github\\STAT\\statassets\\templates','template')
-print(game_template)
-
-new_game_dict = {'name': '','description':'', 'has_counters': False,
-                'counters': {}, 'has_actors': False,
-                'actor_default_path':'', 'default_actors':[],
-                'has_assets': False, 'asset_default_path':'',
-                'default_assets':[], 'has_events': False, 
-                'event_default_path':'', 'default_events':[],
-                'has_effects': False, 'effect_default_path':'',
-                'default_effects':[],'icon':'',
-                  'save_files_path':'', 'has_turns':False,
-                'turn_type':'', 'start_turn':0}
-
-
-template_check = check_template_bool(new_game_dict, 'C:\\Users\\strip\\Documents\\github\\STAT\\statassets\\templates')
-print(template_check)
+if(fetch_success == True):
+    print (object_names)
+else:
+    print(error_message)
